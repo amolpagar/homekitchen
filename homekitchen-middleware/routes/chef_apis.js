a7e666f84dfa68b207fee380de6da07deaa7f226
amolpagar <pagaramol22@gmail.com>
2023-03-26 22:18:21 -0400

    corrected api path

--- homekitchen-middleware/routes/chef_apis.js
+++ homekitchen-middleware/routes/chef_apis.js
@@ -3,14 +3,14 @@ var chef_router = express.Router();
 const { kitchens_collection, menus_collection, closeConnection } = require('../db/mongoModule');
 const { ObjectId } = require('mongodb');
 
-chef_router.post("/v1/chef/kitchen", async (req, res) => {
+chef_router.post("/kitchen", async (req, res) => {
     const { name, address, contact, schedule } = req.body;
     const newKitchen = { name, address, contact, schedule };
     const result = await kitchens_collection.insertOne(newKitchen);
     res.json(result.ops[0]);
 });
 
-chef_router.put("/v1/chef/kitchen/:kitchen_id", async (req, res) => {
+chef_router.put("/kitchen/:kitchen_id", async (req, res) => {
     const { name, address, contact, schedule } = req.body;
     const kitchenId = ObjectId(req.params.kitchen_id);
     const result = await kitchens_collection.findOneAndUpdate(
@@ -21,20 +21,20 @@ chef_router.put("/v1/chef/kitchen/:kitchen_id", async (req, res) => {
     res.json(result.value);
 });
 
-chef_router.get("/v1/chef/kitchen/:kitchen_id", async (req, res) => {
+chef_router.get("/kitchen/:kitchen_id", async (req, res) => {
     const kitchenId = ObjectId(req.params.kitchen_id);
     const result = await kitchens_collection.findOne({ _id: kitchenId });
     res.json(result);
 });
 
-chef_router.delete("/v1/chef/kitchen/:kitchen_id", async (req, res) => {
+chef_router.delete("/kitchen/:kitchen_id", async (req, res) => {
     const kitchenId = ObjectId(req.params.kitchen_id);
     const result = await kitchens_collection.findOneAndDelete({ _id: kitchenId });
     res.json(result.value);
 });
 
 // Menu APIs
-chef_router.post("/v1/chef/kitchen/:kitchen_id/menu", async (req, res) => {
+chef_router.post("/kitchen/:kitchen_id/menu", async (req, res) => {
     const { itemName, itemPhoto, calories, price, isAvailable, availableQuantity, description, spiceLevel } = req.body;
     const kitchenId = ObjectId(req.params.kitchen_id);
     const newMenu = { kitchenId, itemName, itemPhoto, calories, price, isAvailable, availableQuantity, description, spiceLevel };
@@ -42,7 +42,7 @@ chef_router.post("/v1/chef/kitchen/:kitchen_id/menu", async (req, res) => {
     res.json(result.ops[0]);
 });
 
-chef_router.put("/v1/chef/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
+chef_router.put("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
     const { itemName, itemPhoto, calories, price, isAvailable, availableQuantity, description, spiceLevel } = req.body;
     const kitchenId = ObjectId(req.params.kitchen_id);
     const menuId = ObjectId(req.params.menu_id);
@@ -54,14 +54,14 @@ chef_router.put("/v1/chef/kitchen/:kitchen_id/menu/:menu_id", async (req, res) =
     res.json(result.value);
 });
 
-chef_router.get("/v1/chef/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
+chef_router.get("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
     const kitchenId = ObjectId(req.params.kitchen_id);
     const menuId = ObjectId(req.params.menu_id);
     const result = await menus_collection.findOne({ _id: menuId, kitchenId });
     res.json(result);
 });
 
-chef_router.delete("/v1/chef/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
+chef_router.delete("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
     const kitchenId = ObjectId(req.params.kitchen_id);
     const menuId = ObjectId(req.params.menu_id);
     const result = await menus_collection.findOneAndDelete({ _id: menuId, kitchenId });
