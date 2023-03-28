18751f6ca1790a6ce8b2eb980ff0e4c7a5ce8381
amolpagar <pagaramol22@gmail.com>
2023-03-27 20:26:30 -0400

    updated url

--- homekitchen-middleware/db/constants.js
+++ homekitchen-middleware/db/constants.js
@@ -1,4 +1,4 @@
-const MONGODB_CONNECT_URL = "mongodb+srv://mongodbadminuser:mongodbadminuser@cluster0.23il6ss.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_CONNECT_URL = "mongodb+srv://";
 
 module.exports = {
     MONGODB_CONNECT_URL,
