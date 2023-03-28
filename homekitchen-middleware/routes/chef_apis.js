71dc44bbf1952392927065f14e55c999e3147b7e
amolpagar <pagaramol22@gmail.com>
2023-03-27 21:33:17 -0400

    added postman project for testing

--- homekitchen-middleware/routes/chef_apis.js
+++ homekitchen-middleware/routes/chef_apis.js
@@ -7,15 +7,15 @@ chef_router.post("/kitchen", async (req, res) => {
     const { name, address, contact, schedule } = req.body;
     const newKitchen = { name, address, contact, schedule };
     kitchens_collection.insertOne(newKitchen)
-    .then(function (data) {
-        if (data) {          
-          return res.status(201).send(data);
-        }
-      })
-      .catch(function (err) {
-        console.log(err);
-        return res.status(500).json({ message: err });
-      });
+        .then(function (data) {
+            if (data) {
+                return res.status(201).send(data);
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+            return res.status(500).json({ message: err });
+        });
 });
 
 chef_router.put("/kitchen/:kitchen_id", async (req, res) => {
@@ -46,8 +46,15 @@ chef_router.post("/kitchen/:kitchen_id/menu", async (req, res) => {
     const { itemName, itemPhoto, calories, price, isAvailable, availableQuantity, description, spiceLevel } = req.body;
     const kitchenId = ObjectId(req.params.kitchen_id);
     const newMenu = { kitchenId, itemName, itemPhoto, calories, price, isAvailable, availableQuantity, description, spiceLevel };
-    const result = await menus_collection.insertOne(newMenu);
-    res.json(result.ops[0]);
+    menus_collection.insertOne(newMenu).then(function (data) {
+        if (data) {
+            return res.status(201).send(data);
+        }
+    })
+        .catch(function (err) {
+            console.log(err);
+            return res.status(500).json({ message: err });
+        });
 });
 
 chef_router.put("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
@@ -72,8 +79,16 @@ chef_router.get("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
 chef_router.delete("/kitchen/:kitchen_id/menu/:menu_id", async (req, res) => {
     const kitchenId = ObjectId(req.params.kitchen_id);
     const menuId = ObjectId(req.params.menu_id);
-    const result = await menus_collection.findOneAndDelete({ _id: menuId, kitchenId });
-    res.json(result.value);
+    menus_collection.findOneAndDelete({ _id: menuId, kitchenId })
+        .then(function (data) {
+            if (data) {
+                return res.status(201).send(data);
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+            return res.status(500).json({ message: err });
+        });    
 });
 
 module.exports = chef_router;
